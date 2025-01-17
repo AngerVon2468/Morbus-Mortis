6cb81c8692e9d165c77ab2679bba2f642f5ca32f
AngerVon2468 <136974526+AngerVon2468@users.noreply.github.com>
2025-01-17 14:21:20 +1100

    Initial Commit

---
+++ src/main/java/ruby/matty/morbus_mortis/MorbusMortis.java
@@ -0,0 +1,9 @@
+package ruby.matty.morbus_mortis;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MorbusMortis.MOD_ID)
+public class MorbusMortis {
+
+    public static final String MOD_ID = "morbus_mortis";
+}
